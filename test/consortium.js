d2c8f3ea4bfa724d0594e839543d4aec401dd415
ditarud <ditarud@miuandes.cl>
2018-06-10 17:05:54 -0400

    Archivos iniciales

---
+++ test/consortium.js
@@ -0,0 +1,17 @@
+var consortium = artifacts.require("./Consortium.sol");
+
+contract('Consortium', accounts => {
+
+
+  it("should add a new member", () => {
+    return Consorcio.new().then(instance => {
+      return instance.addNewMember.call({ from: accounts[4], value: 500 });
+    }).then( was_added => {
+      assert.equal(was_added, true, "Was not possible to add new member");
+    });
+});
+
+
+
+
+}
